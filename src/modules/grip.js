aab40543cac114a59d788ae3268d1db5092cdabe
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-11-06 16:12:55 -0500

    Adding grip modules

---
+++ src/modules/grip.js
@@ -0,0 +1,82 @@
+const jayap = {
+  name: 'Jayap',
+  type: 1,
+  dmgMod: 0,
+  speed: 0.917,
+  img: '',
+};
+
+const korb = {
+  name: 'Korb',
+  type: 0,
+  dmgMod: 28,
+  speed: 0.783,
+  img: '',
+};
+
+const kroostra = {
+  name: 'Kroostra',
+  type: 1,
+  dmgMod: 14,
+  speed: 0.850,
+  img: '',
+};
+
+const kwath = {
+  name: 'Kwath',
+  type: 0,
+  dmgMod: 14,
+  speed: 0.850,
+  img: '',
+};
+
+const laka = {
+  name: 'Laka',
+  type: 0,
+  dmgMod: 0,
+  speed: 0.917,
+  img: '',
+};
+
+const peye = {
+  name: 'Peye',
+  type: 0,
+  dmgMod: -4,
+  speed: 1,
+  img: '',
+};
+
+const seekalla = {
+  name: 'Seekalla',
+  type: 1,
+  dmgMod: -4,
+  speed: 1,
+  img: '',
+};
+
+const shtung = {
+  name: 'Shtung',
+  type: 1,
+  dmgMod: 28,
+  speed: 0.783,
+  img: '',
+};
+
+const plagueAkwin = {
+  name: 'Plague Akwin',
+  type: 0,
+  dmgMod: -2,
+  speed: 	0.950,
+  img: '',
+};
+
+const plagueBokwin = {
+  name: 'Plague Bokwin',
+  type: 1,
+  dmgMod: 7,
+  speed: 0.883,
+  img: '',
+};
+  
+export { jayap, korb, kroostra, kwath, laka, peye, seekalla, shtung, plagueAkwin, plagueBokwin };
+  
