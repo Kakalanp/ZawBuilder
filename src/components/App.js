46bcf1da5897fb4f076436297839fcb716a7d82f
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2023-08-14 14:17:00 -0500

    Rendering grip images

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import grips from '../modules/grip'
+import links from '../modules/link'
 
 function App() {
-  return (
-    <div/>
-  );
+  return(
+    <div>
+      <h2>Grips:</h2>
+      {grips.map(element => {
+        console.log(element)
+        return (
+          <div key={element.name}>
+            <p>{element.name}</p>
+            < img src={require('../assets/images/grip/' + element.name.toLowerCase().split(' ').join('-') + '.png')} alt={element.name.toLowerCase().split(' ').join('-')} />
+          </div>
+        );
+      })}
+      {/* <h2>Links:</h2>
+      {links.map(element => {
+        console.log(element)
+        return (
+          <div key={element.name}>
+            <p>{element.name}</p>
+            < img src={require('../assets/images/link/' + element.name.toLowerCase().split(' ').join('-') + '.png')} alt={element.name.toLowerCase().split(' ').join('-')} />
+          </div>
+        );
+      })} */}
+    </div>
+  )
 }
 
 export default App;
