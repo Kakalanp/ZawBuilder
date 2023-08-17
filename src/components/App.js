17fe218c22e06bb534a05d4d432113310d58ba38
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2023-08-17 08:14:02 -0500

    Using useState hook for Zaw

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,68 @@
+import React, { useState } from 'react'
 import './App.css';
 import grips from '../modules/grip';
 import links from '../modules/link';
 import strikes from '../modules/strike';
 
-function OpenPieceSelection(piece) {
-  const element = document.getElementById(`${piece}-selector`).classList.remove('disabled');
-}
-
-function ClosePieceSelection(piece) {
-  const element = document.getElementById(`${piece}-selector`).classList.add('disabled');
-}
-
 function App() {
+
+  const [zawStats, setZawStats] = useState({
+    strike: {
+      name: '',
+      imgSrc: '',
+    },
+    grip: {
+      name: '',
+      imgSrc: '',
+    },
+    link: {
+      name: '',
+      imgSrc: '',
+    }
+  });
+
+  function OpenPieceSelection(piece) {
+    document.getElementById(`${piece}-selector`).classList.remove('disabled');
+    document.getElementById('builder').classList.add('disabled');
+  }
+
+  function ClosePieceSelection(piece, part) {
+    document.getElementById(`${piece}-selector`).classList.add('disabled');
+    document.getElementById('builder').classList.remove('disabled');
+
+    setZawStats({...zawStats, [piece]: {
+        name: part.name,
+        imgSrc: require('../assets/images/' + piece + '/' + part.img + '.png'),
+    }});
+}
+
   return(
     <div>
       <div id='builder'>
         <div className='builder' onClick={() => OpenPieceSelection('strike')}>
           <div>
-            <img id='strike-img'></img>
+            <img id='strike-img' src={zawStats.strike.imgSrc}></img>
           </div>
           <div>
-            <h3 id='strike-name'>NONE</h3>
+            <h3 id='strike-name'>{zawStats.strike.name !== '' ? zawStats.strike.name : 'NONE'}</h3>
             <p>Strike</p>
           </div>
         </div>
         <div className='builder' onClick={() => OpenPieceSelection('grip')}>
           <div>
-            <img id='grip-img'></img>
+            <img id='grip-img' src={zawStats.grip.imgSrc}></img>
           </div>
           <div>
-            <h3 id='grip-name'>NONE</h3>
+            <h3 id='grip-name'>{zawStats.grip.name !== '' ? zawStats.grip.name : 'NONE'}</h3>
             <p>Grip</p>
           </div>
         </div>
         <div className='builder' onClick={() => OpenPieceSelection('link')}>
           <div>
-            <img id='link-img'></img>
+            <img id='link-img' src={zawStats.link.imgSrc}></img>
           </div>
           <div>
-            <h3 id='link-name'>NONE</h3>
+            <h3 id='link-name'>{zawStats.link.name !== '' ? zawStats.link.name : 'NONE'}</h3>
             <p>Link</p>
           </div>
         </div>
@@ -46,33 +70,33 @@ function App() {
       <div>
         <div id='strike-selector' className='disabled'>
           <h2>Strikes:</h2>
-          {strikes.map(element => {
+          {strikes.map(part => {
             return (
-              <div key={element.name} onClick={() => ClosePieceSelection('strike')}>
-                <p>{element.name}</p>
-                < img src={require('../assets/images/strike/' + element.img + '.png')} alt={element.name} />
+              <div key={part.name} onClick={() => ClosePieceSelection('strike', part)}>
+                <p>{part.name}</p>
+                < img src={require('../assets/images/strike/' + part.img + '.png')} alt={part.name} />
               </div>
             );
           })}
         </div>
         <div id='grip-selector' className='disabled'>
           <h2>Grips:</h2>
-          {grips.map(element => {
+          {grips.map(part => {
             return (
-              <div key={element.name} onClick={() => ClosePieceSelection('grip')}>
-                <p>{element.name}</p>
-                < img src={require('../assets/images/grip/' + element.img + '.png')} alt={element.name} />
+              <div key={part.name} onClick={() => ClosePieceSelection('grip', part)}>
+                <p>{part.name}</p>
+                < img src={require('../assets/images/grip/' + part.img + '.png')} alt={part.name} />
               </div>
             );
           })}
         </div>
         <div id='link-selector' className='disabled'>
           <h2>Links:</h2>
-          {links.map(element => {
+          {links.map(part => {
             return (
-              <div key={element.name} onClick={() => ClosePieceSelection('link')}>
-                <p>{element.name}</p>
-                < img src={require('../assets/images/link/' + element.img + '.png')} alt={element.name} />
+              <div key={part.name} onClick={() => ClosePieceSelection('link', part)}>
+                <p>{part.name}</p>
+                < img src={require('../assets/images/link/' + part.img + '.png')} alt={part.name} />
               </div>
             );
           })}
